ef8a40093ded91e4f9d0e4fbdad4f9846e9c59ec
alan <alanvega002@gmail.com>
2020-08-07 23:28:51 +0000

    Stripe checkout component added

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51HDlIyLRiYQvUJcsrDB8p5THikhsAjZmI3onOJ1grzNn0WDzo9ld4MV88LnGRylqeE1FSGSBMpUOk9bR148XupFY00g3CsyCnA';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Tgthr Wear"
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is ${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
