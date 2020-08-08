4ea38980f3420ea7f94e86483dc20abe128744bb
alan <alanvega002@gmail.com>
2020-08-08 00:46:17 +0000

    Added mock credit card data for testing

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
@@ -17,8 +18,7 @@ const StripeCheckoutButton = ({ price }) => {
       name="Tgthr Wear"
       billingAddress
       shippingAddress
-      image="https://sendeyo.com/up/d/f3eb2117da"
-      description={`Your total is ${price}`}
+      description={`Your total is $${price}`}
       amount={priceForStripe}
       panelLabel="Pay Now"
       token={onToken}
