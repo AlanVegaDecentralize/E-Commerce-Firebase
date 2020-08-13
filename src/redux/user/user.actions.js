2c84c8a2a0802733a05a66cb5d9adde15942f6d4
alan <alanvega002@gmail.com>
2020-08-13 01:42:19 +0000

    Created email sign in saga, removed from app component

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -9,27 +9,17 @@ export const googleSignInStart = () => ({
   type: UserActionTypes.GOOGLE_SIGN_IN_START,
 });
 
-export const googleSignInSuccess = (user) => ({
-  type: UserActionTypes.GOOGLE_SIGN_IN_SUCCESS,
-  payload: user,
-});
-
-export const googleSignInFailure = (error) => ({
-  type: UserActionTypes.GOOGLE_SIGN_IN_FAILURE,
-  payload: error,
-});
-
 export const emailSignInStart = (credentials) => ({
   type: UserActionTypes.EMAIL_SIGN_IN_START,
   payload: credentials,
 });
 
-export const emailSingInSuccess = (user) => ({
-  type: UserActionTypes.EMAIL_SIGN_IN_SUCCESS,
+export const signInSuccess = (user) => ({
+  type: UserActionTypes.SIGN_IN_SUCCESS,
   payload: user,
 });
 
-export const emailSingInFailure = (error) => ({
-  type: UserActionTypes.EMAIL_SIGN_IN_FAILURE,
+export const signInFailure = (error) => ({
+  type: UserActionTypes.SIGN_IN_FAILURE,
   payload: error,
 });
