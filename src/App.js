0680253b7ba16b8e071560fad28e3f726e337f87
alan <alanvega002@gmail.com>
2020-08-14 11:38:16 +0000

    Refactored sign-in/sign-up components to use react hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // Routing
 import { Switch, Route, Redirect } from 'react-router-dom';
 // Base components
@@ -8,55 +8,40 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
-// Firebase
-import {
-  auth,
-  createUserProfileDocument,
-  // addCollectionAndDocuments,
-} from './firebase/firebase.utils';
+
 // Redux
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 // Redux config
 import { checkUserSession } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
-import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 
 import './App.css';
 
-class App extends React.Component {
-  unsubscribeFromAuth = null;
-
+const App = ({ checkUserSession, currentUser }) => {
   // Google Auth allows for persistent sign in
-  componentDidMount() {
-    const { checkUserSession } = this.props;
+  useEffect(() => {
     checkUserSession();
-  }
+  }, [checkUserSession]);
 
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route exact path="/checkout" component={CheckoutPage} />
-          <Route
-            exact
-            path="/signin"
-            render={() =>
-              this.props.currentUser ? (
-                <Redirect to="/" />
-              ) : (
-                <SignInAndSignUpPage />
-              )
-            }
-          />
-        </Switch>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route exact path="/checkout" component={CheckoutPage} />
+        <Route
+          exact
+          path="/signin"
+          render={() =>
+            currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
+          }
+        />
+      </Switch>
+    </div>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
@@ -64,8 +49,7 @@ const mapStateToProps = createStructuredSelector({
 
 // Dispatch calls the action to change redux store
 const mapDispatchToProps = (dispatch) => ({
-  // setCurrentUser: (user) => dispatch(setCurrentUser(user)),
-  checkUserSession: () => [dispatch(checkUserSession())],
+  checkUserSession: () => dispatch(checkUserSession()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
