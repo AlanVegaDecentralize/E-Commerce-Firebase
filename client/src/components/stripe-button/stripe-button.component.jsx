5d5a8299c05f0d618da43c55b589e39aac93505d
alan <alanvega002@gmail.com>
2020-10-13 23:49:35 -0700

    cart clears item on purchase completion

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,45 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
-import axios from 'axios';
+import { connect } from 'react-redux';
+import { clearCart } from '../../redux/cart/cart.actions';
 
-const StripeCheckoutButton = ({ price }) => {
+const StripeCheckoutButton = ({ price, clearCart }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_b7a3hFL5nC3qlBCZ6bQACpez00gyMMP52H';
 
-  const onToken = token => {
-    axios({
-      url: 'payment',
-      method: 'post',
-      data: {
-        amount: priceForStripe,
-        token: token
-      }
-    })
-      .then(response => {
-        alert('succesful payment');
-      })
-      .catch(error => {
-        console.log('Payment Error: ', JSON.parse(error));
-        alert(
-          'There was an issue with your payment! Please make sure you use the provided credit card.'
-        );
-      });
-  };
+  const onToken = () => {
+    clearCart()
+    alert("Payment succesful!")
+  }
+
+  // const onToken = token => {
+  //   axios({
+  //     url: 'payment',
+  //     method: 'post',
+  //     data: {
+  //       amount: priceForStripe,
+  //       token: token
+  //     }
+  //   })
+  //     .then(response => {
+  //       alert('Succesful payment!');
+  //     })
+  //     .catch(error => {
+  //       console.log('Payment Error: ', JSON.parse(error));
+  //       alert(
+  //         'There was an issue with your payment! Please make sure you use the provided credit card information.'
+  //       );
+  //     });
+  // };
 
   return (
-    <StripeCheckout
-      label='Pay Now'
-      name='CRWN Clothing Ltd.'
-      billingAddress
-      shippingAddress
-      image='https://svgshare.com/i/CUz.svg'
-      description={`Your total is $${price}`}
-      amount={priceForStripe}
-      panelLabel='Pay Now'
-      token={onToken}
-      stripeKey={publishableKey}
-    />
-  );
+    <>
+      <StripeCheckout
+        label='Pay Now'
+        name='Wave-Mart Clothing LLC'
+        billingAddress
+        shippingAddress
+        // image='https://svgshare.com/i/CUz.svg'
+        description={`Your total is $${price}`}
+        amount={priceForStripe}
+        panelLabel='Pay Now'
+        token={onToken}
+        stripeKey={publishableKey}
+      />
+    </>
+  )
 };
 
-export default StripeCheckoutButton;
+const mapDispathToProps = (dispatch) => ({
+  clearCart: () => dispatch(clearCart())
+})
+
+export default connect(null, mapDispathToProps)(StripeCheckoutButton);
